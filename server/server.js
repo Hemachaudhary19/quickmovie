e14e1aedd35a67d4eff3c9c59b5326ce396aa01d
Hemachaudhary19 <hemac1872@gmail.com>
2025-10-06 12:32:51 +0530

    fix showRouter.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRouter.js";
 
 dotenv.config();
 
@@ -21,9 +22,8 @@ app.use(clerkMiddleware());
 // const functions = [];
 
 app.get("/", (req, res) => res.send("server is Live"));
-
-
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/show",showRouter)
 
 app.listen(port, () =>
   console.log(`server listening at http://localhost:${port}`)
